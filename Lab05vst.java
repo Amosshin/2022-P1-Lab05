ebe2e320b272e7a131d199b3fecca58859ca9752
Amos Shin <amos.shin@vtsd.com>
2022-11-01 09:16:46 -0400

    5

--- Lab05vst.java
+++ Lab05vst.java
@@ -17,6 +17,95 @@ public class Lab05vst extends Applet
         int x2 = 990;
         int y2 = 640;
         g.drawRect(10,10,width,height);
-    }
+        //
+      // int x=25;
+       //int y=6;
+      /// int x3=990;
+       // int y3=25;
+        for(int k = 0; k< 35;k++){
+            g.drawLine(x1,y1,x2,y2);
+            x1 += 28;
+            y2 -= 18;
+
+        }
+
+  x1 = 990;
+        x2 = 10;
+        y1 = 640;
+        y2 = 640;
+        for(int k = 0; k< 35;k++) {
+            g.drawLine(x1, y1, x2, y2);
+            x1 -= 28;
+            y2 -= 18;
+        }
+
+x1= 10;
+        x2=990;
+        y1 = 10;
+        y2 = 10;
+        for(int k = 0; k< 35;k++) {
+            g.drawLine(x1, y1, x2, y2);
+            x1 += 28;
+            y2 += 18;
+        }
+
+
+        x1 = 990;
+        y1 = 10;
+        x2 = 10;
+        y2 = 10;
+        for(int k = 0; k< 35;k++) {
+            g.drawLine(x1, y1, x2, y2);
+            x1 -= 28;
+            y2 += 18;
+
+        }
+        width = 500;
+        height = 308;
+        x1 = 250;
+        y1 = 171;
+        x2 = 750;
+        y2 = 171;
+        g.drawRect(250,171,width,height);
+        for(int k = 0; k< 40;k++){
+            g.drawLine(x1,y1,x2,y2);
+            x1 += 12.4;
+            y2 += 8.8;
+
+        }
+        x1 = 250;
+        y1 = 171;
+        x2 = 750;
+        y2 = 171;
+
+        for(int k = 0; k< 40;k++) {
+            g.drawLine(x1, y1, x2, y2);
+            x1 -= 12.4;
+            y2 += 8.8;
+        }
+        x1 = 250;
+        y1 = 171;
+        x2 = 750;
+        y2 = 171;
+
+        for(int k = 0; k< 40;k++) {
+            g.drawLine(x1, y1, x2, y2);
+            x1 -= 12.4;
+            y2 -= 8.8;
+        }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+    }
+//}
 
--- Temp.java
+++ Temp.java
@@ -1,2 +1,27 @@
 public class Temp {
-}
+    public static void main(String arg[])
+    {
+        for(int k=1; k <= 5; k++) {
+            for(int w = 0; w<k; w++) {
+
+                System.out.print("*");
+            }
+            System.out.println();
+
+
+
+
+
+
+
+
+            }
+        }
+
+
+
+
+
+    }
+
+
